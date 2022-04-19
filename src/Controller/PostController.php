34494948b63d52fb66e53991d9bc0f444f85eb9e
Realdens <denis.eleyehou@hotmail.fr>
2022-04-19 09:10:26 +0200

    MAJ formulaire Post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use App\Form\PostType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
@@ -38,6 +41,17 @@ class PostController extends AbstractController
                 
                 Aenean tortor purus, ornare id eros at, blandit porttitor lacus. In tristique velit et nibh sodales imperdiet. Duis sit amet augue molestie, posuere leo eget, laoreet purus. Sed in mattis purus. Nunc aliquet elit a lacus ultrices egestas. Fusce mollis ipsum vitae mi. '
             ],
+        ]);
+    }
+
+    #[Route("/post/add", name: 'post_add')]
+    public function addPost(Request $request): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+
+        return $this->render('post/add.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
