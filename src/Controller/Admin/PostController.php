c6306190e3240fdcffe781cb9dbd073cda96c506
Realdens <denis.eleyehou@hotmail.fr>
2022-05-03 16:09:52 +0200

    Fin de la trame de cours blog

---
+++ src/Controller/Admin/PostController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\PostRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/admin/post', name: 'admin_post_')]
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+        
+        return $this->render('admin/post/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    
+    #[Route("/add", name:"add")]
+     public function addPost(Request $request, ManagerRegistry $doctrine): Response
+    {       
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+        //dd($form);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //$em = $this->getDoctrine()->getManager();
+            $post->setUser($this->getUser());
+            $post->setActive(true);
+            $em = $doctrine->getManager();
+            $em->persist($post);
+            $em->flush();
+            return $this->redirectToRoute('admin_post_index');
+        }
+
+        return $this->render('admin/post/add.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Ajout d\'un article',
+        ]);        
+    }
+
+    #[Route("/update/{id}", name:"update")]
+    public function updatePost(Post $post, Request $request, ManagerRegistry $doctrine): Response
+   {
+       $form = $this->createForm(PostType::class, $post);
+
+       $form->handleRequest($request);
+       if ($form->isSubmitted() && $form->isValid()) {
+           //$em = $this->getDoctrine()->getManager();
+           $em = $doctrine->getManager();
+           $em->persist($post);
+           $em->flush();
+           return $this->redirectToRoute('admin_post_index');
+       }
+
+       return $this->render('admin/post/add.html.twig', [
+           'form' => $form->createView(),
+           'title' => 'Modification d\'un article',
+       ]);        
+   }
+
+   #[Route("/delete/{id}", name:"delete")]
+    public function delete(Post $post, ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $em->remove($post);
+        $em->flush();
+        $this->addFlash('success', 'Article supprimé !');
+        return $this->redirectToRoute('admin_post_index');
+    }
+
+    #[Route("/activate/{id}", name:"activate")]
+    public function activate(Post $post, ManagerRegistry $doctrine): Response
+    {
+        $post->setActive(($post->getActive()) ? false : true);
+        $em = $doctrine->getManager();
+        $em->flush();
+        return new Response("true");
+    }
+}
